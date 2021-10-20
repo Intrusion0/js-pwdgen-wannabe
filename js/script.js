83eae907aec1d449bd513339ccfb30e6b305649e
Mario <lombardomario01@gmail.com>
2021-10-20 16:08:35 +0200

    created files

---
+++ js/script.js
@@ -0,0 +1,6 @@
+/* password generator 
+Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina “La password assegnata è: ” nomecognomecolorepreferito21
+*/
